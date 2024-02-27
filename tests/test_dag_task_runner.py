ad3f4506d6591774d2600f4ff0f07980084e2e12
Tiffany Barth <tiffany.barth@invitae.com>
2024-02-27 14:22:38 -0800

    added additional test for multi parent nodes and updated python version

--- tests/test_dag_task_runner.py
+++ tests/test_dag_task_runner.py
@@ -8,31 +8,49 @@ def create_dict_from_stdout(result):
         # make sure it isn't the empty line at the end of the output
         if line:
             node, timestamp = line.split(", Timestamp: ")
-            result_dict[node.split(": ")[1]] = float(timestamp)
+            node_id = node.split(": ")[1]
+            if node_id in result_dict:
+                result_dict[node_id].append(float(timestamp))
+            else:
+                result_dict[node_id] = [float(timestamp)]
     return result_dict
 
 def test_dag_task_runner():
     runner = CliRunner()
     result = runner.invoke(main, ['--dag_json', 'data/dag_example.json'])
     result_dict = create_dict_from_stdout(result)
-    assert round(result_dict['B'] - result_dict['A'], 1) == 5
-    assert round(result_dict['C'] - result_dict['A'], 1) == 7
-    assert round(result_dict['G'] - result_dict['A'], 1) == 17
-    assert round(result_dict['E'] - result_dict['B'], 1) == 2
-    assert round(result_dict['D'] - result_dict['C'], 1) == 10
-    assert round(result_dict['F'] - result_dict['E'], 1) == 1
+    assert round(result_dict['B'][0] - result_dict['A'][0], 1) == 5
+    assert round(result_dict['C'][0] - result_dict['A'][0], 1) == 7
+    assert round(result_dict['G'][0] - result_dict['A'][0], 1) == 17
+    assert round(result_dict['E'][0] - result_dict['B'][0], 1) == 2
+    assert round(result_dict['D'][0] - result_dict['C'][0], 1) == 10
+    assert round(result_dict['F'][0] - result_dict['E'][0], 1) == 1
 
 def test_combo_long_and_short_running_vertices():
     runner = CliRunner()
     result = runner.invoke(main, ['--dag_json', 'data/dag_example_2.json'])
     result_dict = create_dict_from_stdout(result)
     # ensure long-running vertex executes at the correct time relative to start node
-    assert round(result_dict['G'] - result_dict['A'], 1) == 30
-    assert round(result_dict['H'] - result_dict['G'], 1) == 30
+    assert round(result_dict['G'][0] - result_dict['A'][0], 1) == 30
+    assert round(result_dict['H'][0] - result_dict['G'][0], 1) == 30
     # ensure short-running vertex also executes at the correct time relative to the start node
-    assert round(result_dict['B'] - result_dict['A'], 1) == 2
-    assert round(result_dict['E'] - result_dict['B'], 1) == 2
-    assert round(result_dict['F'] - result_dict['E'], 1) == 1
-    assert round(result_dict['I'] - result_dict['F'], 1) == 1
-    assert round(result_dict['J'] - result_dict['I'], 1) == 1
-    assert round(result_dict['K'] - result_dict['J'], 1) == 1
+    assert round(result_dict['B'][0] - result_dict['A'][0], 1) == 2
+    assert round(result_dict['E'][0] - result_dict['B'][0], 1) == 2
+    assert round(result_dict['F'][0] - result_dict['E'][0], 1) == 1
+    assert round(result_dict['I'][0] - result_dict['F'][0], 1) == 1
+    assert round(result_dict['J'][0] - result_dict['I'][0], 1) == 1
+    assert round(result_dict['K'][0] - result_dict['J'][0], 1) == 1
+
+def test_vertices_with_multiple_parents():
+    '''
+    Here we want to ensure that a DAG that has a non-starting node with multiple parents, such as node D in the example
+    below, will print D twice (once at 3 seconds elapsed, and again at 10 seconds elapsed):
+    A--(5)--B--(3)--D
+     \             /
+      C----(3)----/
+    '''
+    runner = CliRunner()
+    result = runner.invoke(main, ['--dag_json', 'data/multiple_parent_nodes.json'])
+    result_dict = create_dict_from_stdout(result)
+    assert round(result_dict['D'][0] - result_dict['A'][0], 1) == 3
+    assert round(result_dict['D'][1] - result_dict['A'][0], 1) == 8
