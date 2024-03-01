2648226e283a1c09c7bed03d1d1d484adccab1c6
Tiffany Barth <tiffany.barth@invitae.com>
2024-02-29 23:19:50 -0800

    simplified script

--- README.md
+++ README.md
@@ -43,9 +43,6 @@ Node: D, Timestamp: 1709063307.895016
 
 To run tests, simply run `pytest` in the root directory
 
-## Notes
-There's an additional task runner (scripts/dag_task_runner_2.py) which was my initial solution. In this script, I first flattened the data into a sorted list and then made the task schedule. I left it in for now since I thought it might be interesting to discuss the pros & cons of that approach.
-
 ## Assumptions
 
 The task runner assumes that the json files are in the following format and that they are true DAGs without cycles.
--- scripts/dag_task_runner.py
+++ scripts/dag_task_runner.py
@@ -36,22 +36,24 @@ def main(dag_json: str):
         if dag_json_obj[node_id].get("start"):
             start_node_id = node_id
     start_time = time.monotonic()
-    spawn_task_runner([Task(0, start_node_id)], dag_json_obj, scheduler, start_time)
-
-def prepare_tasks(edges):
-    return [Task(edge_value, edge_id) for edge_id, edge_value in edges.items()]
-
-def spawn_task_runner(tasks, dag_json_obj, scheduler, start_time):
-    def print_node_and_time(node_id):
-        print(f'Node: {node_id}, Timestamp: {time.time()}', file=sys.stdout)
-        breadth_first_search(node_id, dag_json_obj, scheduler, time.monotonic())
-    for task in tasks:
-        scheduler.enterabs(start_time + task.run_time, 1, print_node_and_time, (task.node_id,))
+    create_task_runner({start_node_id: 0}, dag_json_obj, scheduler, start_time)
     scheduler.run()
 
+def print_node_and_time(node_id):
+    print(f'Node: {node_id}, Timestamp: {time.time()}', file=sys.stdout)
+
+def create_task_runner(tasks, dag_json_obj, scheduler, start_time):
+    for node_id, task_run_time in tasks.items():
+        scheduler.enterabs(start_time + task_run_time, 1, print_node_and_time, (node_id,))
+        # breadth_first_search(node_id, dag_json_obj, scheduler, start_time + task_run_time)
+
+        child_tasks = dag_json_obj[node_id]["edges"]
+        create_task_runner(child_tasks, dag_json_obj, scheduler, start_time + task_run_time)
+    # return scheduler.queue
+
 def breadth_first_search(current_node_id, dag_json_obj, scheduler, start_time):
-    tasks = prepare_tasks(dag_json_obj[current_node_id]["edges"])
-    spawn_task_runner(tasks, dag_json_obj, scheduler, start_time)
+    if tasks := dag_json_obj[current_node_id]["edges"]:
+        create_task_runner(tasks, dag_json_obj, scheduler, start_time)
 
 
 if __name__ == "__main__":
